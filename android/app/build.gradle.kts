ce3f57e98cc41d2db8252940121e7c89ba165be1
Navid Zaman Khan <navid166418@gmail.com>
2026-08-08 16:52:27 +0600

    build: update android compileSdk version to 36

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.the_shelf"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
